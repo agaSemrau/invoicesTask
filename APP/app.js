01801fdf64115323cb11e1c875dd0ec96a0bda37
agaSemrau <agnieszka.semrau@wp.pl>
2016-04-25 23:00:24 +0200

    creating table - unfinished

--- APP/app.js
+++ APP/app.js
@@ -5,3 +5,47 @@ $(function () {
     });
 });
 
+$.fn.serializeObject = function()
+{
+    var o = {FordrHaver: "Olsen Rer"};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+var finalArray=[];
+$(function() {
+    $('.form-horizontal').submit(function () {
+        var finalObject = ($('.form-horizontal').serializeObject());
+        console.log(finalObject);
+        finalArray.push(finalObject);
+        console.log(finalArray);
+
+        $('#example').DataTable( {
+            data: finalArray,
+            columns: [
+                { title: "Fordr.haver" },
+                { title: "Faktnr/kto.nr" },
+                { title: "Skyldner" },
+                { title: "Belop" },
+                { title: "Rente" }
+            ]
+        } );
+
+
+        return false;
+
+    });
+
+});
+
+
+
