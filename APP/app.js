8c20d488a84f012014a7a044abc13e646857fb74
Agnieszka Semrau <agnieszka.semrau-jozwiak@lppsa.com>
2016-04-26 14:05:40 +0200

    creating table - unfinished

--- APP/app.js
+++ APP/app.js
@@ -1,51 +1,89 @@
-$(function () {
-    $(".datepicker").datepicker({
-        autoclose: true,
-        todayHighlight: true
-    });
-});
+$('document').ready(function () {
 
-$.fn.serializeObject = function()
-{
-    var o = {FordrHaver: "Olsen Rer"};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-var finalArray=[];
-$(function() {
-    $('.form-horizontal').submit(function () {
-        var finalObject = ($('.form-horizontal').serializeObject());
-        console.log(finalObject);
-        finalArray.push(finalObject);
+    $(".datepicker").datepicker();
+    var finalArray = [];
+    var totalArray = [];
+
+    $.fn.serializeObject = function () {
+        finalArray = this.serializeArray();
+        console.log(this.name);
         console.log(finalArray);
 
-        $('#example').DataTable( {
-            data: finalArray,
-            columns: [
-                { title: "Fordr.haver" },
-                { title: "Faktnr/kto.nr" },
-                { title: "Skyldner" },
-                { title: "Belop" },
-                { title: "Rente" }
-            ]
-        } );
+        $.each(finalArray, function () {
+            console.log(this.name);
+        })
+
+        var result = {};
+        for (var i=0; i<finalArray.length; i++) {
+            result[finalArray[i].name] = finalArray[i].value;
+        }
 
 
-        return false;
+
+//result
+        console.log(result);
+        var fvnr = result.fakturanr;
+
+        var myFinalObject = {
+            FordrHaver: "Olsen Rer",
+            Faktnr: fvnr,
+
+
+        }
+        //$.each(finalArray, function () {
+        //    if (o[this.name] !== undefined) {
+        //        if (!o[this.name].push) {
+        //            o[this.name] = [o[this.name]];
+        //        }
+        //        o[this.name].push(this.value || '');
+        //    } else {
+        //        o[this.name] = this.value || '';
+        //    }
+
+        //});
+
+
+        //return o;
+    };
+
+    $(function () {
+        $('.form-horizontal').submit(function () {
+            ($('.form-horizontal').serializeObject());
+
+            var valuesArray = finalArray.map(function (a) {
+                return a.value;
+            });
+            totalArray.push(valuesArray);
+            console.log(totalArray);
+
+
+            $('#example').DataTable({
+                data: totalArray,
+                columns: [
+                    {title: "Fordr.haver",},
+                    {title: "Faktnr/kto.nr"},
+                    {title: "Skyldner"},
+                    {title: "Belop"},
+                    {title: "Rente"},
+                    {title: "Delete"}
+                ],
+                //columnDefs: [
+                //    {
+                //        "targets": [ 2 ],
+                //        "visible": false,
+                //        "searchable": false
+                //    },
+                //    {
+                //        "targets": [ 3 ],
+                //        "visible": false
+                //    }
+                //]
+            });
+
+            return false;
+        });
 
     });
-
 });
-
 
 
