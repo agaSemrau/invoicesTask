e1c08ccbad1b6943a5e9c3e6d465aa2ae0a22ae3
Agnieszka Semrau <agnieszka.semrau-jozwiak@lppsa.com>
2016-04-27 11:00:55 +0200

    working on validation

--- APP/app.js
+++ APP/app.js
@@ -2,8 +2,12 @@ $('document').ready(function () {
     var result = {};
     var totalArrayToDisplay = [];
     var table;
+    var table1;
 
-    $(".datepicker").datepicker();
+    $(".datepicker").datepicker({
+        format: 'dd/mm/yy'
+
+    });
 
     $.fn.serializeObject = function () {
         var finalArray = this.serializeArray();
@@ -25,87 +29,95 @@ $('document').ready(function () {
         this.FarInkas = FarInkas || "No data provided";
     }
 
-        $('.form-horizontal').submit(function created() {
+    $('.form-horizontal').submit(function created() {
 
-            if ( $.fn.dataTable.isDataTable('#example')){
-                table.destroy();
+        if ($.fn.dataTable.isDataTable('#example')) {
+            table.destroy();
+        }
+
+        ($('.form-horizontal').serializeObject());
+        var myFinalObject = new Debtor(result.fakturanr, result.belop, result.rentesats, result.dato, result.farfallsdato, result.inkassovarsel, result.farinkas);
+        var arrayToDisplay = [];
+        for (key in myFinalObject) {
+            if (myFinalObject.hasOwnProperty(key)) {
+                var value = myFinalObject[key];
+                arrayToDisplay.push(value);
             }
-
-            ($('.form-horizontal').serializeObject());
-            var myFinalObject = new Debtor(result.fakturanr, result.belop, result.rentesats);
-            var arrayToDisplay = [];
-            for (key in myFinalObject) {
-                if (myFinalObject.hasOwnProperty(key)) {
-                    var value = myFinalObject[key];
-                    arrayToDisplay.push(value);
+        }
+        totalArrayToDisplay.push(arrayToDisplay);
+        table = $('#example').DataTable({
+            data: totalArrayToDisplay,
+            columns: [
+                {title: "Fordr.haver"},
+                {title: "Faktnr/kto.nr"},
+                {title: "Skyldner"},
+                {title: "Belop"},
+                {title: "Rente"},
+                {
+                    title: "Delete", "defaultContent": "<div id='del'></div>", render: function () {
                 }
+                }
+            ]
+        });
+
+        $('tr>td>#del').on('click', function () {
+            var theRaw = $(this.closest('tr'));
+            var rowindex = $(this).closest("tr").index();
+            delete totalArrayToDisplay[rowindex];
+            theRaw.remove();
+
+            //table.data();
+            //if(totalArrayToDisplay == 0){table.destroy();}
+            //table.clear();           // clear all the rows ( not really necessary since we call empty() at the end)
+            //table.destroy();              // remove table enhancements
+
+        });
+
+        $('#example tbody').on('click', 'tr', function () {
+            var rowindex = $(this).closest("tr").index();
+            var datesArray = [];
+            datesArray.push(totalArrayToDisplay[rowindex]);
+
+            if ($.fn.dataTable.isDataTable('#example1')) {
+                table1.destroy();
             }
-            totalArrayToDisplay.push(arrayToDisplay);
 
-            table = $('#example').DataTable({
-                data: totalArrayToDisplay,
+            table1 = $('#example1').DataTable({
+                 bFilter: false,
+                bInfo: false,
+                paging: false,
+                data: datesArray,
                 columns: [
-                    {title: "Fordr.haver"},
-                    {title: "Faktnr/kto.nr"},
-                    {title: "Skyldner"},
-                    {title: "Belop"},
-                    {title: "Rente"},
-                    {title: "Delete", "defaultContent":"<div id='del'></div>", render: function(){
-                    }},
-                    {title: "Dato", "visible": false},
-                    {title: "Farfallsdato", "visible": false},
-                    {title: "Inkassovarsel", "visible": false},
-                    {title: "Inkassovarsel-Farfallsdato", "visible": false}
-
-
-                ]
-            });
-            table = $('#example1').DataTable({
-                data: totalArrayToDisplay,
-                columns: [
-                    {title: "Fordr.haver", "visible": false},
-                    {title: "Faktnr/kto.nr", "visible": false},
-                    {title: "Skyldner", "visible": false},
-                    {title: "Belop", "visible": false},
-                    {title: "Rente", "visible": false},
-                    {title: "Delete", "visible": false, "defaultContent":"<div id='del'></div>", render: function(){
-                    }},
+                    {title: "Fordr.haver", "visible": false, "defaultContent": "No data provided"},
+                    {title: "Faktnr/kto.nr", "visible": false, "defaultContent": "No data provided"},
+                    {title: "Skyldner", "visible": false, "defaultContent": "No data provided"},
+                    {title: "Belop", "visible": false, "defaultContent": "No data provided"},
+                    {title: "Rente", "visible": false, "defaultContent": "No data provided"},
+                    {
+                        title: "Delete",
+                        "visible": false,
+                        "defaultContent": "<div id='del'></div>",
+                        render: function () {
+                        }
+                    },
                     {title: "Dato"},
                     {title: "Farfallsdato"},
                     {title: "Inkassovarsel"},
                     {title: "Inkassovarsel-Farfallsdato"}
-
-
                 ]
             });
-            $('tr>td>#del').on('click', function () {
-                    var theRaw = $(this.closest('tr'))
-                    var rowindex = $(this).closest("tr").index();
 
-                    delete totalArrayToDisplay[rowindex]
-                    theRaw.remove();
+            $("#dialog").dialog();
 
-                //table.data();
-                //if(totalArrayToDisplay == 0){table.destroy();}
-                //table.clear();           // clear all the rows ( not really necessary since we call empty() at the end)
-                //table.destroy();              // remove table enhancements
-
-            });
-
-            $('#example tbody').on( 'click', 'tr', function () {
-                $( "#dialog" ).dialog();
-
-                if ( $(this).hasClass('selected')) {
-                    $(this).removeClass('selected');
-                }
-                else {
-                    table.$('tr.selected').removeClass('selected');
-                    $(this).addClass('selected');
-                }
-            });
-            return false;
+            if ($(this).hasClass('selected')) {
+                $(this).removeClass('selected');
+            }
+            else {
+                table.$('tr.selected').removeClass('selected');
+                $(this).addClass('selected');
+            }
         });
-
-
+        return false;
+    });
 });
 
---
+++ APP/validate.js
@@ -0,0 +1,29 @@
+$('.form-horizontal').validate({
+    rules: {
+        dato: {
+            required: true,
+            pattern:  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.input-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.input-group').removeClass('has-error');
+    },
+    success: function(element) {
+        console.log('sukces')
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+
+
